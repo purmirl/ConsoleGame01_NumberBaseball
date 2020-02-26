9b13a2fab4ebd953941a45d1cbd3a407206c9e52
PeTrA-Korea <gmlrnjs1994@gmail.com>
2020-02-26 22:21:36 +0900

    scanf_s function !

--- src/main.c
+++ src/main.c
@@ -16,6 +16,22 @@ int main(){
 	int computer_number_b = randomNumber();
 	int computer_number_c = randomNumber();
 	
+	int player_number_a = -1;
+	int player_number_b = -1;
+	int player_number_c = -1;
+	
+	scanf_s("%d", &player_number_a, 1); /* scanf_s("format string", address of value, size of value) */
+	scanf_s("%d", &player_number_b, 1);
+	scanf_s("%d", &player_number_c, 1);
+	
+	printf("computer number a : %d\n", computer_number_a);
+	printf("computer number b : %d\n", computer_number_b);
+	printf("computer number c : %d\n", computer_number_c);
+	
+	printf("player number a : %d\n", player_number_a);
+	printf("player number b : %d\n", player_number_b);
+	printf("player number c : %d\n", player_number_c);
+	
 	exitProgram();
 	return 0;
 	
